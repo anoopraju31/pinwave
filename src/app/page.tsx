909600e678f360bc856c613c6dbee979c3f59c10
anoopraju31 <anoopraju31@gmail.com>
2023-08-06 21:42:01 +0530

    Sidebar component : initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { Sidebar } from '@/components'
+
 export default function Home() {
 	return (
 		<main className='h-[200vh] flex justify-center items-center'>
-			{' '}
-			Hello World{' '}
+			<Sidebar />
 		</main>
 	)
 }
