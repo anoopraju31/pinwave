42c621266ec0272754fa31525af8a1d857a1ed7d
anoopraju31 <anoopraju31@gmail.com>
2023-08-06 20:47:07 +0530

    Search bar component : initial

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link'
 import { RxHamburgerMenu } from 'react-icons/rx'
+import { SearchBar } from '.'
 
 const Navbar = () => {
 	return (
 		<header className='h-16 px-4 flex items-center justify-between sticky top-0 bg-slate-600 z-50'>
 			<div className='text-lg font-bold'>
 				<Link href='/'> PinWave </Link>
+			</div>
+
+			<div className=''>
+				<SearchBar />
 			</div>
 
 			<div className='block lg:hidden text-lg font-bold'>
