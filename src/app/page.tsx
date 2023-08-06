9a35fc62f489fe9789c664f0c1733766e9ec4e4d
anoopraju31 <anoopraju31@gmail.com>
2023-08-06 20:45:34 +0530

    Navbar : sm styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<main className='w-screen h-screen bg-slate-500 flex justify-center items-center'>
+		<main className='h-[200vh] flex justify-center items-center'>
 			{' '}
 			Hello World{' '}
 		</main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import Link from 'next/link'
+import { RxHamburgerMenu } from 'react-icons/rx'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<header className='h-16 px-4 flex items-center justify-between sticky top-0 bg-slate-600 z-50'>
+			<div className='text-lg font-bold'>
+				<Link href='/'> PinWave </Link>
+			</div>
+
+			<div className='block lg:hidden text-lg font-bold'>
+				<RxHamburgerMenu size={22} />
+			</div>
+		</header>
+	)
 }
 
 export default Navbar
