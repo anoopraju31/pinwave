d27b29266c67fb253fa69d9a3932ff7db685b9cb
anoopraju31 <anoopraju31@gmail.com>
2023-08-06 22:19:06 +0530

    Sidebar component : base styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import { Sidebar } from '@/components'
 
 export default function Home() {
 	return (
-		<main className='h-[200vh] flex justify-center items-center'>
-			<Sidebar />
+		<main className='h-[200vh] relative flex'>
+			<div className=''>
+				<Sidebar />
+			</div>
+
+			<h1 className='w-full h-40 sm:ml-64 lg:ml-0 text-center bg-gray-500'>
+				{' '}
+				Hello world{' '}
+			</h1>
 		</main>
 	)
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Sidebar = () => {
-	return <div>Sidebar</div>
+	return (
+		<div className='w-full sm:w-64 h-[var(--sidebar-height)] z-50 fixed lg:sticky top-16 left-0 bg-black opacity-25'>
+			Sidebar
+		</div>
+	)
 }
 
 export default Sidebar
