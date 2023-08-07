445cf7b0781bcc2f1971c5a25b74f6015a693a14
anoopraju31 <anoopraju31@gmail.com>
2023-08-07 13:35:21 +0530

    Pin card Component : initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,12 @@ import { Sidebar } from '@/components'
 
 export default function Home() {
 	return (
-		<main className='h-[200vh] relative flex bg-orange-300'>
+		<main className='h-[200vh] relative flex'>
 			<div className=''>
 				<Sidebar />
 			</div>
 
-			<h1 className='w-full h-40  lg:ml-0 text-center bg-gray-500'>
-				{' '}
-				Hello world{' '}
-			</h1>
+			<h1 className='w-full h-40  lg:ml-0 text-center '> Hello world </h1>
 		</main>
 	)
 }
---
+++ src/components/PinCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PinCard = () => {
+	return <div>PinCard</div>
+}
+
+export default PinCard
